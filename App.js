76bdc90c8e32df0a8ecc0b8ce5f728061ac33ee0
huseyin pektas <huseyinpektas@huseyin-MacBook-Air.local>
2024-06-03 22:49:07 +0300

    first commit

---
+++ App.js
@@ -0,0 +1,51 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+
+
+function App () {
+  
+
+  return (
+    <SafeAreaView >
+      
+      <ScrollView>
+        <Header />
+        <View>
+         <Text>
+          burası yeni oluşturduğum projemdir
+         </Text>
+          
+         
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+
+
+export default App;
